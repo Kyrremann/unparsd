b6bf24cddff76126e04b19d89ba8044cebff93e3
Kyrre Havik <kyrremann@gmail.com>
2021-10-30 10:17:48 +0200

    feat: statistics for breweries

---
+++ statistics/breweries.go
@@ -0,0 +1,29 @@
+package statistics
+
+import "gorm.io/gorm"
+
+type Brewery struct {
+	Name     string
+	Country  string
+	State    string
+	City     string
+	Beers    string
+	Checkins int
+}
+
+func breweries(db *gorm.DB) ([]Brewery, error) {
+	var breweries []Brewery
+	res := db.
+		Table("breweries").
+		Select("breweries.name, country, city, state, group_concat(beers.name) as beers, count(checkins.beer_id) as checkins").
+		Joins("INNER JOIN beers ON beers.brewery_id == breweries.id").
+		Joins("INNER JOIN checkins ON beers.id == checkins.beer_id").
+		Group("beers.brewery_id").
+		Find(&breweries)
+
+	if res.Error != nil {
+		return nil, res.Error
+	}
+
+	return breweries, nil
+}
---
+++ statistics/breweries_test.go
@@ -0,0 +1,25 @@
+package statistics
+
+import (
+	"testing"
+
+	"github.com/kyrremann/unparsd/parsing"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestBreweries(t *testing.T) {
+	db, err := parsing.LoadJsonIntoDatabase("../fixture/untappd.json")
+	assert.NoError(t, err)
+
+	breweries, err := breweries(db)
+	assert.NoError(t, err)
+	assert.Len(t, breweries, 60)
+
+	var checkins = 0
+	for _, b := range breweries {
+		checkins += b.Checkins
+	}
+	assert.Equal(t, 100, checkins)
+
+	assert.Equal(t, "", breweries)
+}
