b54bc2ae40246c159c2ade992f73928ceda12cf0
Kyrre Havik <kyrremann@gmail.com>
2021-10-31 19:16:20 +0100

    feat: added most per day

--- statistics/global_stats.go
+++ statistics/global_stats.go
@@ -1,12 +1,11 @@
 package statistics
 
 import (
-	"strconv"
-
+	"github.com/kyrremann/unparsd/models"
 	"gorm.io/gorm"
 )
 
-// TODO: Days drinking
+// TODO: days_drinking
 type GlobalStats struct {
 	Checkins     int            `json:"checkins"`
 	UniqueBeers  int            `json:"unique_beers"`
@@ -15,21 +14,58 @@ type GlobalStats struct {
 	Periodes     []PeriodeStats `gorm:"-" json:"years"`
 }
 
-// TODO: most_per_day
+type MostPerDay struct {
+	Count int    `json:"count"`
+	Date  string `json:"date"`
+}
+
 type PeriodeStats struct {
-	Checkins         int            `json:"checkins"`
-	Breweries        int            `json:"breweries"`
-	BreweryCountries int            `json:"brewery_countries"`
-	Venues           int            `json:"venues"`
-	VenueCountries   int            `json:"venue_countries"`
-	Beers            int            `json:"beers"`
-	MaxAbv           float64        `json:"max_abv"`
-	AvgAbv           float64        `json:"avg_abv"`
-	Styles           int            `json:"styles"`
-	StartDate        string         `json:"start_date"`
-	Month            *int           `json:"month,omitempty"`
-	Year             int            `json:"year"`
-	Months           []PeriodeStats `gorm:"-" json:"months,omitempty"`
+	Checkins              int            `json:"checkins"`
+	Breweries             int            `json:"breweries"`
+	BreweryCountries      int            `json:"brewery_countries"`
+	Venues                int            `json:"venues"`
+	VenueCountries        int            `json:"venue_countries"`
+	Beers                 int            `json:"beers"`
+	MaxAbv                float64        `json:"max_abv"`
+	AvgAbv                float64        `json:"avg_abv"`
+	Styles                int            `json:"styles"`
+	StartDate             string         `json:"start_date"`
+	Month                 *string        `json:"month,omitempty"`
+	Year                  string         `json:"year"`
+	Months                []PeriodeStats `gorm:"-" json:"months,omitempty"`
+	MostCheckinsPerDay    MostPerDay     `gorm:"-" json:"most_checkins_per_day"`
+	MostUniqueBeersPerDay MostPerDay     `gorm:"-" json:"most_unique_beers_per_day"`
+}
+
+func MostCheckinsPerDay(db *gorm.DB, year, month string) (MostPerDay, error) {
+	return mostPerDay(db, "count(id) as count,", year, month)
+}
+
+func MostUniqueBeersPerDay(db *gorm.DB, year, month string) (MostPerDay, error) {
+	return mostPerDay(db, "count(DISTINCT(beer_id)) as count,", year, month)
+}
+
+func mostPerDay(db *gorm.DB, selectCounter, year, month string) (MostPerDay, error) {
+	var mostPerDay MostPerDay
+	tx := db.
+		Model(models.Checkin{}).
+		Select(selectCounter +
+			"strftime('%Y-%m-%d', checkins.checkin_at) as date," +
+			"strftime('%Y', checkins.checkin_at) as year," +
+			"strftime('%m', checkins.checkin_at) as month").
+		Group("date").
+		Order("count DESC").
+		Order("date ASC")
+
+	if len(month) > 0 {
+		tx.Where("year = ? AND month = ?", year, month)
+	} else {
+		tx.Where("year = ?", year)
+	}
+
+	res := tx.First(&mostPerDay)
+
+	return mostPerDay, res.Error
 }
 
 func periodeStats(db *gorm.DB, groupBy, year string) ([]PeriodeStats, error) {
@@ -41,7 +77,7 @@ func periodeStats(db *gorm.DB, groupBy, year string) ([]PeriodeStats, error) {
 			"count(DISTINCT(beers.brewery_id)) as breweries," +
 			"count(DISTINCT(beers.id)) as beers," +
 			"count(DISTINCT(beers.type)) as styles," +
-			"max(beers.abv) as max_abv," +
+			"ROUND(max(beers.abv), 2) as max_abv," +
 			"ROUND(avg(beers.abv), 2) as avg_abv," +
 			"count(DISTINCT(checkins.venue_id)) as venues," +
 			"count(DISTINCT(venues.country)) as venue_countries," +
@@ -62,8 +98,27 @@ func periodeStats(db *gorm.DB, groupBy, year string) ([]PeriodeStats, error) {
 	return periodeStats, res.Error
 }
 
-func monthlyStats(db *gorm.DB, year int) ([]PeriodeStats, error) {
-	return periodeStats(db, "month", strconv.Itoa(year))
+func monthlyStats(db *gorm.DB, year string) ([]PeriodeStats, error) {
+	monthly, err := periodeStats(db, "month", year)
+	if err != nil {
+		return nil, err
+	}
+
+	for i, ps := range monthly {
+		mostCheckinsPerDay, err := MostCheckinsPerDay(db, year, *ps.Month)
+		if err != nil {
+			return nil, err
+		}
+
+		monthly[i].MostCheckinsPerDay = mostCheckinsPerDay
+		mostUniqueBeersPerDay, err := MostUniqueBeersPerDay(db, year, *ps.Month)
+		if err != nil {
+			return nil, err
+		}
+		monthly[i].MostUniqueBeersPerDay = mostUniqueBeersPerDay
+	}
+
+	return monthly, nil
 }
 
 func yearlyStats(db *gorm.DB) ([]PeriodeStats, error) {
@@ -72,13 +127,25 @@ func yearlyStats(db *gorm.DB) ([]PeriodeStats, error) {
 		return nil, err
 	}
 
-	for i, year := range yearly {
-		monthly, err := monthlyStats(db, year.Year)
+	for i, pd := range yearly {
+		monthly, err := monthlyStats(db, pd.Year)
 		if err != nil {
 			return nil, err
 		}
 		yearly[i].Months = monthly
 		yearly[i].Month = nil
+
+		mostCheckinsPerDay, err := MostCheckinsPerDay(db, pd.Year, "")
+		if err != nil {
+			return nil, err
+		}
+
+		yearly[i].MostCheckinsPerDay = mostCheckinsPerDay
+		mostUniqueBeersPerDay, err := MostUniqueBeersPerDay(db, pd.Year, "")
+		if err != nil {
+			return nil, err
+		}
+		yearly[i].MostUniqueBeersPerDay = mostUniqueBeersPerDay
 	}
 
 	return yearly, nil
--- statistics/global_stats_test.go
+++ statistics/global_stats_test.go
@@ -19,3 +19,28 @@ func TestAllMy(t *testing.T) {
 	assert.Len(t, globalStats.Periodes, 5)
 	assert.Len(t, globalStats.Periodes[0].Months, 3)
 }
+
+func TestMostCheckinsPerDay(t *testing.T) {
+	db, err := parsing.LoadJsonIntoDatabase("../fixture/untappd.json")
+	assert.NoError(t, err)
+
+	mostCheckinsPerDay, err := MostCheckinsPerDay(db, "2020", "")
+	assert.NoError(t, err)
+	assert.Equal(t, 4, mostCheckinsPerDay.Count)
+	assert.Equal(t, "2020-01-23", mostCheckinsPerDay.Date)
+
+	mostCheckinsPerDay, err = MostCheckinsPerDay(db, "2016", "05")
+	assert.NoError(t, err)
+	assert.Equal(t, 1, mostCheckinsPerDay.Count)
+	assert.Equal(t, "2016-05-15", mostCheckinsPerDay.Date)
+
+	mostUniqueBeersPerDay, err := MostUniqueBeersPerDay(db, "2020", "")
+	assert.NoError(t, err)
+	assert.Equal(t, 3, mostUniqueBeersPerDay.Count)
+	assert.Equal(t, "2020-01-04", mostUniqueBeersPerDay.Date)
+
+	mostUniqueBeersPerDay, err = MostUniqueBeersPerDay(db, "2016", "05")
+	assert.NoError(t, err)
+	assert.Equal(t, 1, mostUniqueBeersPerDay.Count)
+	assert.Equal(t, "2016-05-15", mostUniqueBeersPerDay.Date)
+}
