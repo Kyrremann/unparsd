9831c15d3e3d7cad7673ead1be75f64bc8a33629
Kyrre Havik <kyrremann@gmail.com>
2022-04-15 11:25:30 +0200

    fix: group_concat on newline
    SQLite doesn't support delimiter when using group_concat and distinct,
    so getting the list of beers after we have the other data. This means
    that we now do a look up for each brewery instead of once.

--- statistics/breweries.go
+++ statistics/breweries.go
@@ -19,7 +19,6 @@ func BreweryStats(db *gorm.DB) ([]Brewery, error) {
 		Select("breweries.id as id," +
 			"breweries.name," +
 			"breweries.country," +
-			"group_concat(DISTINCT(beers.name)) as list_of_beers," +
 			"count(checkins.beer_id) as checkins").
 		Joins("LEFT JOIN beers ON beers.brewery_id == breweries.id").
 		Joins("LEFT JOIN checkins ON beers.id == checkins.beer_id").
@@ -28,6 +27,22 @@ func BreweryStats(db *gorm.DB) ([]Brewery, error) {
 
 	if res.Error != nil {
 		return nil, res.Error
+	}
+
+	for i, brewery := range breweries {
+		var beers string
+		res = db.
+			Table("beers").
+			Distinct("name").
+			Select("group_concat(name, '\n') as list_of_beers").
+			Where("beers.brewery_id == ?", brewery.ID).
+			Find(&beers)
+
+		if res.Error != nil {
+			return nil, res.Error
+		}
+
+		breweries[i].ListOfBeers = beers
 	}
 
 	return breweries, nil
