0d4ec771cb860432656f92e44bd48f24cc7c58b6
Kyrre Havik <kyrremann@gmail.com>
2022-12-10 15:35:08 +0100

    feat: using ISO code for French Guiana
    ISO3166alpha2 == GF

--- statistics/countries.go
+++ statistics/countries.go
@@ -11,6 +11,7 @@ type Country struct {
 	Breweries int      `json:"breweries"`
 	Checkins  int      `json:"checkins"`
 	Name      string   `json:"name"`
+	State     string   `json:"state"`
 	Settings  Settings `json:"settings" gorm:"-"`
 }
 
@@ -41,7 +42,7 @@ type ISO3166Alpha2 struct {
 	Query *gountries.Query
 }
 
-func (iso ISO3166Alpha2) getISO3166Alpha2(country string) (string, error) {
+func (iso ISO3166Alpha2) getISO3166Alpha2(country, state string) (string, error) {
 	switch country {
 	case "China / People's Republic of China":
 		return "CN", nil
@@ -55,6 +56,13 @@ func (iso ISO3166Alpha2) getISO3166Alpha2(country string) (string, error) {
 		return "SR", nil
 	case "North Macedonia":
 		return "MK", nil
+	case "France":
+		switch state {
+		case "Guyane":
+			return "GF", nil
+		default:
+			break
+		}
 	}
 
 	gountry, err := iso.Query.FindCountryByName(country)
@@ -70,7 +78,8 @@ func CountryStats(db *gorm.DB) ([]Country, error) {
 		Model(models.Checkin{}).
 		Select("count(checkins.id) as checkins," +
 			"count(DISTINCT(breweries.id)) as breweries," +
-			"breweries.country as name").
+			"breweries.country as name," +
+			"breweries.state as state").
 		Joins("LEFT JOIN beers on checkins.beer_id == beers.id").
 		Joins("LEFT JOIN breweries on beers.brewery_id == breweries.id").
 		Group("breweries.country").
@@ -84,7 +93,7 @@ func CountryStats(db *gorm.DB) ([]Country, error) {
 	}
 	var countries []Country
 	for _, c := range dbCountries {
-		ISO3166Alpha2, err := iso.getISO3166Alpha2(c.Name)
+		ISO3166Alpha2, err := iso.getISO3166Alpha2(c.Name, c.State)
 		if err != nil {
 			return nil, err
 		}
@@ -118,7 +127,7 @@ func CountryStateStats(db *gorm.DB) (map[string]State, error) {
 	}
 	states := make(map[string]State)
 	for _, c := range dbStates {
-		ISO3166Alpha2, err := iso.getISO3166Alpha2(c.Name)
+		ISO3166Alpha2, err := iso.getISO3166Alpha2(c.Country, c.Name)
 		if err != nil {
 			return nil, err
 		}
